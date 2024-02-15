bb6addfb3dbfae30b9c0fa7f81bbf7265f53a771
trisdoan <doanminhtri8183@gmail.com>
2024-02-15 11:06:58 +0700

    [IMP] mail_composer_cc_bcc_account: pre-commit auto fixes

--- mail_composer_cc_bcc/models/ir_mail_server.py
+++ mail_composer_cc_bcc/models/ir_mail_server.py
@@ -2,21 +2,52 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models
+
 from .mail_thread import format_emails
 
+
 class IrMailServer(models.Model):
-    _inherit = 'ir.mail_server'
+    _inherit = "ir.mail_server"
 
-    def build_email(self, email_from, email_to, subject, body, email_cc=None, email_bcc=None, reply_to=False,
-                        attachments=None, message_id=None, references=None, object_id=False, subtype='plain', headers=None,
-                        body_alternative=None, subtype_alternative='plain'):
-            context = self.env.context
+    def build_email(
+        self,
+        email_from,
+        email_to,
+        subject,
+        body,
+        email_cc=None,
+        email_bcc=None,
+        reply_to=False,
+        attachments=None,
+        message_id=None,
+        references=None,
+        object_id=False,
+        subtype="plain",
+        headers=None,
+        body_alternative=None,
+        subtype_alternative="plain",
+    ):
+        context = self.env.context
 
-            res = super().build_email(email_from, email_to, subject, body, email_cc=email_cc, email_bcc=email_bcc, reply_to=reply_to,
-                        attachments=attachments, message_id=message_id, references=references, object_id=object_id, subtype=subtype, headers=headers,
-                        body_alternative=body_alternative, subtype_alternative=subtype_alternative)
-            
-            if context.get('partner_bcc_ids'):
-                  res['Bcc'] = format_emails(context.get('partner_bcc_ids'))
-            
-            return res
+        res = super().build_email(
+            email_from,
+            email_to,
+            subject,
+            body,
+            email_cc=email_cc,
+            email_bcc=email_bcc,
+            reply_to=reply_to,
+            attachments=attachments,
+            message_id=message_id,
+            references=references,
+            object_id=object_id,
+            subtype=subtype,
+            headers=headers,
+            body_alternative=body_alternative,
+            subtype_alternative=subtype_alternative,
+        )
+
+        if context.get("partner_bcc_ids"):
+            res["Bcc"] = format_emails(context.get("partner_bcc_ids"))
+
+        return res
