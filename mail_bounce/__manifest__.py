53dc0e1cdbe925c1ef85faf34936edc4245c1070
trisdoan <doanminhtri8183@gmail.com>
2024-07-10 16:02:01 +0700

    [ADD] mail_bounce

---
+++ mail_bounce/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mail Bounce",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "category": "Social Network",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "version": "17.0.1.0.0",
+    "depends": ["mail", "test_mail"],
+    "data": [
+        "views/res_partner.xml",
+    ],
+}
---
+++ mail_bounce/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+* `Camptocamp <https://www.camptocamp.com>`_
+
+  * Anna <anna.janiszewska@camptocamp.com>
+  * Tri Doan <tridm@trobz.com>
